0373a281e8ca7851a0e35030e1ac21680469ee89
neruneru <neruneru55555@gmail.com>
2020-02-15 18:51:25 +0900

    fix idとタイムスタンプの2つを返すように変更

--- files_list.py
+++ files_list.py
@@ -2,11 +2,17 @@ import requests
 import os
 import json
 
+
 def get_files_list():
     url = "https://slack.com/api/files.list"
-    slack_res = requests.get(url, params = {"token": os.environ["SLACK_TOKEN"]}).json()
+    files = []
+    count = 1000
+    slack_res = requests.get(url, params = {"token": os.environ["SLACK_TOKEN"], "count" : count}).json()
     if slack_res["ok"]:
-        channel_id = [i.get("timestamp") for i in slack_res["files"]]
-        return channel_id
+        timestamp = [file.get("timestamp") for file in slack_res["files"]]
+        file_id = [file.get("id") for file in slack_res["files"]]
+        for i in range(len(slack_res["files"])):
+            files.append({"timestamp": timestamp[i], "file_id": file_id[i]})
+        return files
     else:
         return -1
