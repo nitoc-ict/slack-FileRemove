4c48a84a950cdceaca919bcf398bc63df5f168ff
neruneru <neruneru55555@gmail.com>
2020-02-15 18:50:22 +0900

    fix 受け取る値をリストからidの文字列に変更

--- files_delete.py
+++ files_delete.py
@@ -3,10 +3,11 @@ import os
 import channel_list
 
 
-def delete_files_list(files):
+def delete(file):
     url = "https://slack.com/api/files.delete"
-    for file in files:
-        slack_res = requests.get(
-            url, params={"token": os.environ["SLACK_TOKEN"], "file": file}).json()
+    slack_res = requests.get(
+        url, params={"token": os.environ["SLACK_TOKEN"], "file": file}).json()
     if slack_res["ok"]:
         print("delete files")
+    else:
+        print("failed")
